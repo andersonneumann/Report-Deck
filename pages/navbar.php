d4a1fa27eb4e91bc2990aecb8d8091fc6531df72
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-04-30 12:39:58 -0300

    Otimização HTML - PHP
    Separei partes que se repetem em arquivos únicos para serem incluídos apenas com um comando include, assi otimizando o projeto!

---
+++ pages/navbar.php
@@ -0,0 +1,19 @@
+<?php
+// Escrever comandos aqui
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+   <div class="container">
+      <img src="images/rdlogo.png" width="10%">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="oi oi-menu"></span>Menu
+      </button>
+      <div class="collapse navbar-collapse" id="ftco-nav">
+         <ul class="navbar-nav ml-auto">
+            <li class="nav-item"><a href="index.html" class="nav-link">Início</a></li>
+            <li class="nav-item"><a href="about.html" class="nav-link">Sobre Nós</a></li>
+            <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>VOLTAR</span></a></li>
+         </ul>
+      </div>
+   </div>
+</nav>
--- pages/sign-up.php
+++ pages/sign-up.php
@@ -4,41 +4,9 @@
 
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <title>Report Deck</title>
-      <meta charset="utf-8">
-      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,,500,600,700" rel="stylesheet">
-      <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-      <link rel="stylesheet" href="css/animate.css">
-      <link rel="stylesheet" href="css/owl.carousel.min.css">
-      <link rel="stylesheet" href="css/owl.theme.default.min.css">
-      <link rel="stylesheet" href="css/magnific-popup.css">
-      <link rel="stylesheet" href="css/aos.css">
-      <link rel="stylesheet" href="css/ionicons.min.css">
-      <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-      <link rel="stylesheet" href="css/jquery.timepicker.css">
-      <link rel="stylesheet" href="css/flaticon.css">
-      <link rel="stylesheet" href="css/icomoon.css">
-      <link rel="stylesheet" href="css/style.css">
-   </head>
+   <!-- head.php aqui -->
    <body>
-      <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-         <div class="container">
-            <img src="images/rdlogo.png" width="10%">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="oi oi-menu"></span> Menu
-            </button>
-            <div class="collapse navbar-collapse" id="ftco-nav">
-               <ul class="navbar-nav ml-auto">
-                  <li class="nav-item"><a href="index.html" class="nav-link">Início</a></li>
-                  <li class="nav-item"><a href="about.html" class="nav-link">Sobre Nós</a></li>
-                  <li class="nav-item cta"><a href="contact.html" class="nav-link"><span>VOLTAR</span></a></li>
-               </ul>
-            </div>
-         </div>
-      </nav>
-      <!-- END nav -->
+      <!-- navbar.php aqui -->
       <section class="ftco-section contact-section ftco-degree-bg ">
          <script>
             function validaSenha(){
@@ -90,42 +58,6 @@
             </div>
          </div>
       </section>
-      <footer class="ftco-footer ftco-bg-dark ftco-section">
-         <div class="container">
-            <div class="row">
-               <div class="col-md-12 text-center">
-                  <p>
-                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                     <script>document.write(new Date().getFullYear());</script> | Report Deck
-                     <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                  </p>
-               </div>
-            </div>
-         </div>
-      </footer>
-      <!-- loader -->
-      <div id="ftco-loader" class="show fullscreen">
-         <svg class="circular" width="48px" height="48px">
-            <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/>
-            <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
-         </svg>
-      </div>
-      <script src="js/jquery.min.js"></script>
-      <script src="js/jquery-migrate-3.0.1.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-      <script src="js/jquery.easing.1.3.js"></script>
-      <script src="js/jquery.waypoints.min.js"></script>
-      <script src="js/jquery.stellar.min.js"></script>
-      <script src="js/owl.carousel.min.js"></script>
-      <script src="js/jquery.magnific-popup.min.js"></script>
-      <script src="js/aos.js"></script>
-      <script src="js/jquery.animateNumber.min.js"></script>
-      <script src="js/bootstrap-datepicker.js"></script>
-      <script src="js/jquery.timepicker.min.js"></script>
-      <script src="js/scrollax.min.js"></script>
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
-      <script src="js/google-map.js"></script>
-      <script src="js/main.js"></script>
+      <!-- footer.php aqui -->
    </body>
 </html>
