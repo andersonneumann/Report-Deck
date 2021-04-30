c10b76f5e4bb4e4376792e5a1824e220c7f36026
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-04-30 16:44:24 -0300

    Update navbar.php
    Coloquei um script para alterar o botão no menu para Começar e seu endereço para comecar.php quando o include estiver dentro do index.php

--- pages/navbar.php
+++ pages/navbar.php
@@ -1,7 +1,3 @@
-<?php
-// Escrever comandos aqui
-?>
-
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
    <div class="container">
       <img src="../images/rdlogo.png" width="10%">
@@ -12,8 +8,19 @@
          <ul class="navbar-nav ml-auto">
             <li class="nav-item"><a href="index.php" class="nav-link">Início</a></li>
             <li class="nav-item"><a href="about.php" class="nav-link">Sobre Nós</a></li>
-            <li class="nav-item cta"><a href="comecar.php" class="nav-link"><span>VOLTAR</span></a></li>
+            <li class="nav-item cta"><a id="buttonR" href="voltar.php" class="nav-link start"><span id="return">VOLTAR</span></a></li>
          </ul>
       </div>
-   </div>
+   </div>   
 </nav>
+<script>
+   console.log(window.location.pathname);
+   if(window.location.pathname === "/pages/index.php"){
+      var botao = document.querySelector("#return");
+      botao.innerHTML = "COMEÇAR!"
+      document.getElementById("buttonR").href="comecar.php"; 
+   }else{
+      botao.innerHTML = "VOLTAR"
+      document.getElementById("buttonR").href="voltar.php"; 
+   }
+</script>
