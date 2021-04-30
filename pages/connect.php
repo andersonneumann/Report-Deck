6399f5e27eee910b4b69f4aabb75f3cd7a8e9eab
koenins <koenins@pop-os.localdomain>
2021-04-30 17:14:56 -0300

    Integração ao DB

---
+++ pages/connect.php
@@ -0,0 +1,8 @@
+<?php
+    define('HOST', 'localhost');
+    define('USER', 'report_deck');
+    define('PASSWD', '123');
+    define('DB', 'ReportDeck');
+    
+    $conn = mysqli_connect(HOST, USER, PASSWD, DB) or die ('Não foi possivel conectar.');
+?>
