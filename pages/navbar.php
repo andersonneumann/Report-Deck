f3ee34d69a0d5bad9163b9ef1431375ca7515dd2
Matheus Henrique Fraga Berni <mtbernioficial@gmail.com>
2021-05-03 11:10:07 -0300

    JS
    Ajustado JS

--- pages/navbar.php
+++ pages/navbar.php
@@ -7,7 +7,7 @@
       <div class="collapse navbar-collapse" id="ftco-nav">
          <ul class="navbar-nav ml-auto">
             <li class="nav-item"><a href="index.php" class="nav-link">Início</a></li>
-            <li class="nav-item"><a href="about.php" class="nav-link">Sobre Nós</a></li>
+            <li class="nav-item"><a href="index.php#about" class="nav-link">Sobre Nós</a></li>
             <li class="nav-item cta"><a id="buttonR" href="voltar.php" class="nav-link start"><span id="return">VOLTAR</span></a></li>
          </ul>
       </div>
