a1911ca714eb06aed2d109e034031e5cb57749a2
andersonneumann <anderson.lara@tecpuc.com.br>
2021-05-14 16:02:44 -0300

    Ajustado layout para mobile

--- js/pageUsuario/usuario.js
+++ js/pageUsuario/usuario.js
@@ -5,24 +5,57 @@ $(document).ready(function(){
 });
 
 function loadPage(){
+  $(".appendContentClick").html("");
   exibirMenuDeOpcoes();
 }
 
 function exibirMenuDeOpcoes(){
-
+  $(".appendContentClick").html("");
   var nomeUsuario = "[User]";
 
-  var itens = [["Mapa de Ocorrências", "Nova Ocorrência"], ["Visualizar Ocorrências", "Meu Perfil"], ["Configurações", "Sair"]];
+  var itens = [["Mapa de Ocorrências", "Nova Ocorrência"], ["Visualizar Ocorrências", "<br>Meu Perfil"], ["Configurações", "Sair"]];
   var imagens = [["mapaBranco.png", "novaOcorrenciaBranco.png"],["visualizarBranco.png", "perfilBranco.png"],["configBranco.png", "sairBranco.png"]]
+  var action = [["exibirMapaDeOcorrencias()","criarNovaOcorrencia()"],["visualizarOcorrencias()","exibirPerfil()"],["exibirConfigs()","sair()"]]
+
   var val = "";
-  val+= '<br><h1 class="text-center textoTemaEscuro">Bem Vindo '+ nomeUsuario +'!</h1>'
+  val+= '<br><h2 class="text-center textoTemaEscuro">Bem Vindo '+ nomeUsuario +'!</h2>'
   val += '<ul class="nav justify-content-center">'
   for (var i = 0; i<3; i++){
-    val += '<li class="nav-item"><div class="navegacao"><img src="../images/navBar/' + imagens[i][0] + '" class="rounded mx-auto d-block imgNavBar">'
-    val += '<h2 class="descricaoNavBar textoTemaEscuro">' + itens[i][0] + '</h2></div></li>'
-    val += '<div class="navegacao"><img src="../images/navBar/' + imagens[i][1] + '" class="rounded mx-auto d-block imgNavBar">'
-    val += '<h2 class="descricaoNavBar textoTemaEscuro">' + itens[i][1] + '</h2></div>'
+    val += '<a onclick="' + action[i][0] + '"><li class="nav-item"><div class="navegacao"><img src="../images/navBar/' + imagens[i][0] + '" class="rounded mx-auto d-block imgNavBar">'
+    val += '<h6 class="descricaoNavBar textoTemaEscuro">' + itens[i][0] + '</h6></div></li></a>'
+    val += '<a onclick="' + action[i][1] + '"><div class="navegacao"><img src="../images/navBar/' + imagens[i][1] + '" class="rounded mx-auto d-block imgNavBar">'
+    val += '<h6 class="descricaoNavBar textoTemaEscuro">' + itens[i][1] + '</h6></div></a>'
   }
   val += '</ul>'
   $(".appendContentClick").append(val);
+}
+
+function exibirMapaDeOcorrencias(){
+  $(".appendContentClick").html("");
+  alert("Exibindo mapa de Ocorrências");
+}
+
+function criarNovaOcorrencia(){
+  $(".appendContentClick").html("");
+  alert("Criando nova ocorrência");
+}
+
+function visualizarOcorrencias(){
+  $(".appendContentClick").html("");
+  alert("Exibindo ocorrencias");
+}
+
+function exibirPerfil(){
+  $(".appendContentClick").html("");
+  alert("Exibindo perfil");
+}
+
+function exibirConfigs(){
+  $(".appendContentClick").html("");
+  alert("Exibindo configs");
+}
+
+function sair(){
+  $(".appendContentClick").html("");
+  alert("Saindo");
 }
