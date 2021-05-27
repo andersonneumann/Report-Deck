e888c4df61e82a693b0109eb3f2df1e601d6064a
brennomeneses <brenno.meneses@tecpuc.com.br>
2021-05-27 19:36:59 -0300

    Banco de dados correto

--- sql/Report deck.sql
+++ sql/Report deck.sql
@@ -1,24 +1,26 @@
+drop database reportdeck;
+
 create database ReportDeck;
 
 use ReportDeck;
 
 -- Cria a tabela cidadão com seus respectivos campos;
 Create table Cidadao(
-	id int not null AUTO_INCREMENT,
-	CPF char(11) not null,
+    CPF char(11) not null,
+	ID int not null,
 	Genero varchar(9) not null,
 	Tema boolean, 
 	Nascimento date not null,
-	Nome_completo varchar(200) not null,
-	Telefone varchar(11),
-	Senha varchar(50)  not null,
+    Nome_completo varchar(200) not null,
+    Telefone varchar(11),
+    Senha varchar(50)  not null,
 	RG char(11)  not null,
-	Email varchar(200) not null,
-	primary key(CPF, ID));
+    Email varchar(200) not null,
+    primary key(CPF, ID));
 
 -- Cria a tabela administrador com seus respectivos campos;    
 Create table Administrador(
-    Codigo int not null ,
+    Codigo int not null,
 	NomeUsuarioADM varchar(50)  not null,
     Senha varchar(50)  not null,
     NomeCompleto varchar(200)  not null,
@@ -38,29 +40,21 @@ create table Crimes(
 
 -- Cria a tabela ocorrência com seus respectivos campos
 create table Ocorrencia(
-	Codigo int primary key,
+	Codigo int auto_increment,
     Titulo varchar(100) not null,
     Crime int not null,
-    DescricaoSuspeito text,
+    grauDoCrime int not null,
+    DescricaoCrime text,
     Observacao text,
+    enderecoOcorrencia varchar(300),
+    Imagem mediumblob,
     DataOcorrencia date,
     HoraOcorrenciaApx time,
     cidadao char(11) not null,
-	administradorResp int not null,
+    primary key(Codigo),
 -- vincula os campos da tabela ocorrência como chave estrangeira de outras tabelas;
     foreign key(Crime) references Crimes(id),
-    foreign key(cidadao) references Cidadao(CPF),
-    foreign key(administradorResp) references Administrador(Codigo)
-);
-
--- Cria a tabela imagem com seus respectivos campos;
-create table imagem(
-	codigo int primary key,
-    codOcorrencia int not null,
-    statusImagem Boolean not null,
-    Imagem mediumblob,
--- vincula as imagens com a ocorrência que pertence;
-    foreign key(codOcorrencia) references Ocorrencia(codigo)
+    foreign key(cidadao) references Cidadao(CPF)
 );
 
 -- Cria a tabela atualização com seus respectivos campos;
@@ -74,5 +68,16 @@ create table Atualizacao(
     foreign key(cidadao) references Cidadao(CPF),
     foreign key(CodOcorrencia) references Ocorrencia(Codigo)
 );
+
+alter table Ocorrencia auto_increment=01;
+
+INSERT INTO `Crimes` (`id`, `Nome`, `Nivel`) VALUES
+(1, 'Assalto', '1'),
+(2, 'Sequestro', '2'),
+(3, 'Homicidio', '3'),
+(4, 'Estupro', '4'),
+(5, 'Arrastão', '2'),
+(6, 'Trafico', '3');
+
 
 
