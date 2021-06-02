30e21e33c792a877215bece14095697848683a25
Eduardo Ribeiro de Souza <eduardo.souza3@tecpuc.com.br>
2021-06-02 10:36:56 -0300

    SQlv1.5.1

--- sql/Report_deck.sql
+++ sql/Report_deck.sql
@@ -1,5 +1,3 @@
-drop database reportdeck;
-
 create database ReportDeck;
 
 use ReportDeck;
@@ -7,20 +5,20 @@ use ReportDeck;
 -- Cria a tabela cidadão com seus respectivos campos;
 Create table Cidadao(
     CPF char(11) not null,
-	ID int not null,
+	id int not null,
 	Genero varchar(9) not null,
-	Tema boolean, 
 	Nascimento date not null,
     Nome_completo varchar(200) not null,
     Telefone varchar(11),
     Senha varchar(50)  not null,
 	RG char(11)  not null,
     Email varchar(200) not null,
-    primary key(CPF, ID));
+    primary key(CPF, id)
+);
 
 -- Cria a tabela administrador com seus respectivos campos;    
 Create table Administrador(
-    Codigo int not null,
+    id int not null,
 	NomeUsuarioADM varchar(50)  not null,
     Senha varchar(50)  not null,
     NomeCompleto varchar(200)  not null,
@@ -29,7 +27,8 @@ Create table Administrador(
     CPF char(11) not null,
     Telefone char(11)  not null,
     Email varchar(200) not null,
-    primary key(Codigo, NomeUsuarioADM));
+    primary key(id, NomeUsuarioADM)
+);
 
 -- Cria a tabela crimes com seus respectivos campos
 create table Crimes(
@@ -40,44 +39,48 @@ create table Crimes(
 
 -- Cria a tabela ocorrência com seus respectivos campos
 create table Ocorrencia(
-	Codigo int auto_increment,
-    Titulo varchar(100) not null,
+	id int auto_increment,
     Crime int not null,
-    grauDoCrime int not null,
+    Titulo varchar(100) not null,
     DescricaoCrime text,
-    Observacao text,
+    estado varchar(2),
+    cidade varchar (100),
+    bairro varchar (100), 
     enderecoOcorrencia varchar(300),
-    Imagem mediumblob,
     DataOcorrencia date,
     HoraOcorrenciaApx time,
+    Imagem mediumblob,
+    ImagemAprovada boolean,
+    apvGenero boolean,
+    apvIdade boolean,
+    OcorrenciaAprovada boolean,
     cidadao char(11) not null,
-    primary key(Codigo),
--- vincula os campos da tabela ocorrência como chave estrangeira de outras tabelas;
+    primary key(id),
+    -- vincula os campos da tabela ocorrência como chave estrangeira de outras tabelas;
     foreign key(Crime) references Crimes(id),
     foreign key(cidadao) references Cidadao(CPF)
 );
 
--- Cria a tabela atualização com seus respectivos campos;
-create table Atualizacao(
-	Codigo int primary key,
-    CodOcorrencia int,
-    Titulo varchar(50),
-    Conteudo text,
-    cidadao char(11) not null,
--- vincula as atualizações com a ocorrência e o cidadão que postou a ocorrência e a atualização
-    foreign key(cidadao) references Cidadao(CPF),
-    foreign key(CodOcorrencia) references Ocorrencia(Codigo)
+-- Cria a tabela GrauProximidadeCrime com seus respectivos campos;
+create table GrauProximidadeCrime(
+	id int auto_increment,
+    descricaoGrau varchar(200),
+    primary key(id)
 );
 
 alter table Ocorrencia auto_increment=01;
+alter table GrauProximidadeCrime auto_increment=01;
 
-INSERT INTO `Crimes` (`id`, `Nome`, `Nivel`) VALUES
+insert into GrauProximidadeCrime (descricaograu) values
+('Eu fui a vítima da ocorrência'),
+('Eu presenciei a ocorrência'),
+('Eu soube da ocorrência'),
+('Eu conheço a vítima da ocorrência');
+
+INSERT INTO Crimes (id, Nome, Nivel) VALUES
 (1, 'Assalto', '1'),
 (2, 'Sequestro', '2'),
 (3, 'Homicidio', '3'),
-(4, 'Estupro', '4'),
+(4, 'Estupro', '3'),
 (5, 'Arrastão', '2'),
 (6, 'Trafico', '3');
-
-
-
