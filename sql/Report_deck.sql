8a4f5933758e04a2399ce722eee8f97bc523fd1f
Eduardo Ribeiro de Souza <eduardo.souza3@tecpuc.com.br>
2021-06-04 12:10:40 -0300

    SQL Hotfix1.5.2

--- sql/Report_deck.sql
+++ sql/Report_deck.sql
@@ -1,3 +1,5 @@
+drop database reportdeck;
+
 create database ReportDeck;
 
 use ReportDeck;
@@ -5,20 +7,20 @@ use ReportDeck;
 -- Cria a tabela cidadão com seus respectivos campos;
 Create table Cidadao(
     CPF char(11) not null,
-	id int not null,
+	ID int not null,
 	Genero varchar(9) not null,
+	Tema boolean, 
 	Nascimento date not null,
     Nome_completo varchar(200) not null,
     Telefone varchar(11),
     Senha varchar(50)  not null,
 	RG char(11)  not null,
     Email varchar(200) not null,
-    primary key(CPF, id)
-);
+    primary key(CPF, ID));
 
 -- Cria a tabela administrador com seus respectivos campos;    
 Create table Administrador(
-    id int not null,
+    Codigo int not null,
 	NomeUsuarioADM varchar(50)  not null,
     Senha varchar(50)  not null,
     NomeCompleto varchar(200)  not null,
@@ -27,8 +29,7 @@ Create table Administrador(
     CPF char(11) not null,
     Telefone char(11)  not null,
     Email varchar(200) not null,
-    primary key(id, NomeUsuarioADM)
-);
+    primary key(Codigo, NomeUsuarioADM));
 
 -- Cria a tabela crimes com seus respectivos campos
 create table Crimes(
@@ -37,50 +38,61 @@ create table Crimes(
     Nivel char(1)
 );
 
--- Cria a tabela ocorrência com seus respectivos campos
-create table Ocorrencia(
-	id int auto_increment,
-    Crime int not null,
-    Titulo varchar(100) not null,
-    DescricaoCrime text,
-    estado varchar(2),
-    cidade varchar (100),
-    bairro varchar (100), 
-    enderecoOcorrencia varchar(300),
-    DataOcorrencia date,
-    HoraOcorrenciaApx time,
-    Imagem mediumblob,
-    ImagemAprovada boolean,
-    apvGenero boolean,
-    apvIdade boolean,
-    OcorrenciaAprovada boolean,
-    cidadao char(11) not null,
-    primary key(id),
-    -- vincula os campos da tabela ocorrência como chave estrangeira de outras tabelas;
-    foreign key(Crime) references Crimes(id),
-    foreign key(cidadao) references Cidadao(CPF)
-);
-
--- Cria a tabela GrauProximidadeCrime com seus respectivos campos;
 create table GrauProximidadeCrime(
-	id int auto_increment,
+	id int not null,
     descricaoGrau varchar(200),
     primary key(id)
 );
 
+-- Cria a tabela ocorrência com seus respectivos campos
+create table Ocorrencia(
+	Codigo int auto_increment,
+    Titulo varchar(100) not null,
+    Crime int not null,
+    grauDoCrime int not null,
+    DescricaoCrime text,
+    Observacao text,
+    enderecoOcorrencia varchar(300),
+    Imagem mediumblob,
+    ImagemAprovada boolean,
+    DataOcorrencia date,
+    HoraOcorrenciaApx time,
+    apvGenero boolean,
+    apvIdade boolean,
+    cidadao char(11) not null,
+    ocorrenciaAprovada boolean,
+    primary key(Codigo),
+-- vincula os campos da tabela ocorrência como chave estrangeira de outras tabelas;
+    foreign key(Crime) references Crimes(id),
+    foreign key(grauDoCrime) references GrauProximidadeCrime(id),
+    foreign key(cidadao) references Cidadao(CPF)
+);
+
+-- Cria a tabela atualização com seus respectivos campos;
+create table Atualizacao(
+	Codigo int primary key,
+    CodOcorrencia int,
+    Titulo varchar(50),
+    Conteudo text,
+    cidadao char(11) not null,
+-- vincula as atualizações com a ocorrência e o cidadão que postou a ocorrência e a atualização
+    foreign key(cidadao) references Cidadao(CPF),
+    foreign key(CodOcorrencia) references Ocorrencia(Codigo)
+);
+
 alter table Ocorrencia auto_increment=01;
-alter table GrauProximidadeCrime auto_increment=01;
 
-insert into GrauProximidadeCrime (descricaograu) values
-('Eu fui a vítima da ocorrência'),
-('Eu presenciei a ocorrência'),
-('Eu soube da ocorrência'),
-('Eu conheço a vítima da ocorrência');
-
-INSERT INTO Crimes (id, Nome, Nivel) VALUES
+INSERT INTO `Crimes` (`id`, `Nome`, `Nivel`) VALUES
 (1, 'Assalto', '1'),
 (2, 'Sequestro', '2'),
 (3, 'Homicidio', '3'),
-(4, 'Estupro', '3'),
+(4, 'Estupro', '4'),
 (5, 'Arrastão', '2'),
 (6, 'Trafico', '3');
+
+insert into GrauProximidadeCrime (id,descricaograu) values
+(1, 'Eu fui a vítima da ocorrência'),
+(2, 'Eu presenciei a ocorrência'),
+(3, 'Eu soube da ocorrência'),
+(4, 'Eu conheço a vítima da ocorrência');
+
